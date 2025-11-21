a9f0f755a54764f87ac1dc5f0223f636547417f1
Gaurav270901 <shimpigaurav27s@gmail.com>
2025-11-21 19:19:44 +0530

    basket microservices added

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,5 @@
 ﻿
+
 namespace Basket.API.Basket.StoreBasket
 {
     public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -12,15 +13,17 @@ namespace Basket.API.Basket.StoreBasket
             RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
         }
     }
-    public class StoreBasketCommandHandler :
+    public class StoreBasketCommandHandler(IBasketRepository repository) :
         ICommandHandler<StoreBasketCommand, StoreBasketResult>
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
             ShoppingCart cart = command.Cart;
+            await repository.StoreBasket(cart , cancellationToken);
+
             //update or create cart
 
-            return new StoreBasketResult("gaurev");
+            return new StoreBasketResult(cart.UserName);
         }
     }
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,28 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
-  //add servces to the container
+
+//add servces to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter(); 
+builder.Services.AddMediatR(config =>
+{
+       config.RegisterServicesFromAssembly(assembly);
+       config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+       config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+
+});
+
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opt.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository , BasketRepository>();
 var app = builder.Build();
 
 // configure the http request pipeline
+
+app.MapCarter();
 app.Run();
